e91bc84defb3052387d38f44921cc8e47ded2646
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-09-04 22:34:12 +0300

    add boostrap and the templates

--- Gallary/settings.py
+++ Gallary/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'photos'
+    'photos',
+    'bootsrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
