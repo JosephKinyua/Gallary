21038bff69c5b64a0f4b1226e49b88b97199de83
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-09-05 20:01:40 +0300

    create model location

--- Gallary/settings.py
+++ Gallary/settings.py
@@ -78,8 +78,14 @@ WSGI_APPLICATION = 'Gallary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':'picture',
+        'USER':'tash',
+        'PASSWORD':'wanstop',
+        'HOST': 'localhost',
+        'PORT': '',
+
+        
     }
 }
 
@@ -108,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
