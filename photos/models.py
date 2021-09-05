773872230ee837db3b1542fc10163ef9f77a58ee
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-09-05 20:04:53 +0300

    add the save and delete functionality

--- photos/models.py
+++ photos/models.py
@@ -3,4 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Location(models.Model):
-     location = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+
+    def saveLocation(self):
+        self.save()
+
+    @classmethod
+    def deleteLocation(cls, id):
+        cls.objects.filter(id=id).delete()
